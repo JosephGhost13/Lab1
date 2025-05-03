64d98372697079dd9f11b13e921b5535328a9851
JosephGhost13 <iossarris@uth.gr>
2025-05-03 15:59:33 +0300

    ergasia

---
+++ homework.pl
@@ -0,0 +1,11 @@
+
+my $dna = "ATGCTAGC";
+
+my $length = length($dna);
+print"length:$length\n";
+
+my $lowercase = lc($dna);
+print"lowercase:$lowercase\n";
+
+my $reversed = reverse($dna);
+print"reversed:$reversed\n";
